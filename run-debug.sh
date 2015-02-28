97a26e72717facd9fe54e5c66f271d30a0e2491a
Benjamin Michalski <benjamin.michalski@gmail.com>
2015-02-28 20:14:42 +0100

    Refacto.

--- run-debug.sh
+++ run-debug.sh
@@ -2,11 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-APT_CACHER_NG_DATA_EXISTS=`docker inspect --format="{{ .Id }}" apt-cacher-ng-data 2> /dev/null`
-
-if [ -z "$APT_CACHER_NG_DATA_EXISTS" ]
-then
-  docker run -d -v /var/cache/apt-cacher-ng -v /var/log/apt-cacher-ng --name apt-cacher-ng-data debian
-fi
+$DIR/common-run.sh
 
 docker run --volumes-from apt-cacher-ng-data -it bmichalski/apt-cacher-ng bash
